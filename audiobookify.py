a3f027487cf2c37e5331e1136e6a8d62cdf0906c
Henrik Karlsson <sven.henrik.karlsson@gmail.com>
2021-08-19 15:22:13 +0200

    Cleaned code and clarifying

--- audiobookify.py
+++ audiobookify.py
@@ -1,4 +1,6 @@
-#%%
+# %% 
+# The pdf that this thing has been delevoped on can be found here: https://ia903205.us.archive.org/11/items/prime_intellect/prime_intellect.pdf
+
 import pdfreader
 import re
 from spellchecker import SpellChecker
@@ -6,16 +8,14 @@ import os
 import pyttsx3
 from time import sleep
 
-# %%
+# %% Settings
 
-#The pdf that this thing has been delevoped on can be found here: https://ia903205.us.archive.org/11/items/prime_intellect/prime_intellect.pdf
-
-VOICE_NAME = 'Karen' #Choose which voice to use
+TEST = False # Change to True if you want to save sample mp3s of all available voices on your system.
+VOICE_NAME = 'Karen' # Choose which voice to use. If TEST == True, then a sample of all available voices on your system will be saved to the project location when you run the script. After listening in, you can make your pick and assign VOICE_NAME.
 root_dir = os.path.dirname(os.path.abspath(__file__))+'/'
-filename = [f for f in os.listdir(root_dir) if '.pdf' in f[-4:]][0] #Gets the first pdf file in the project directory. 
+filename = [f for f in os.listdir(root_dir) if '.pdf' in f[-4:]][0] # Gets the first pdf file in the project directory. 
 fd = open(root_dir + filename, "rb")
 viewer = pdfreader.SimplePDFViewer(fd)
-TEST = False #Change to true if you want to save sample mp3s of all available voices on your system.
 
 #%% Reading pdf and saving to txt file.
 i = 0
@@ -40,25 +40,25 @@ f.close()
 # %% Cleaning up string. (This is adapted for one specific pdf and need to be adapted according to the pdf you use as input.)
 s2 = re.sub('The Metamorphosis of Prime Intellect: Chapter 1http://www.kuro5hin.org/prime-intellect/mopiall.html[0-9]+ of 13423/03/08 23:03', ' ', s)
 
-s2 = re.sub('(?<=[a-z])(?=[A-Z])',' ', s2) #Separating chapter title endings from begining of chapter text
-s2 = re.sub('  \* (?=Chapter .+:   )',' \n\n', s2) #Spacing chapter beginnings
-s2 = re.sub('(?<=(\.|\?|\!))(?=\"?[A-z])',' ', s2) #Adding missing spaces after sentences
-s2 = re.sub('(?<=(\,|\;))(?=[A-z])',' ', s2) #Adding missing spaces in sentences
-s2 = re.sub('(\*|\>)', '\n- ', s2) #Standardizing dialogue character
+s2 = re.sub('(?<=[a-z])(?=[A-Z])',' ', s2) # Separating chapter title endings from begining of chapter text
+s2 = re.sub('  \* (?=Chapter .+:   )',' \n\n', s2) # Spacing chapter beginnings
+s2 = re.sub('(?<=(\.|\?|\!))(?=\"?[A-z])',' ', s2) # Adding missing spaces after sentences
+s2 = re.sub('(?<=(\,|\;))(?=[A-z])',' ', s2) # Adding missing spaces in sentences
+s2 = re.sub('(\*|\>)', '\n- ', s2) # Standardizing dialogue character
 s2 = re.sub('(?<=\")(?=\")', ' \n', s2) # Spacing dialogue
 
-s2 = re.sub(',\"', '\",', s2) #Relocating quotes that ended up on wrong sides of commas
-s2 = re.sub('\? \"(?=[A-Z])','\?\" ', s2)  #Relocating quotes that ended up on wrong sides of spaces
+s2 = re.sub(',\"', '\",', s2) # Relocating quotes that ended up on wrong sides of commas
+s2 = re.sub('\? \"(?=[A-Z])','\?\" ', s2)  # Relocating quotes that ended up on wrong sides of spaces
 
-names = ['caroline', 'timothy', 'mamba', 'glasslike'] #These are some words that occur in the book that SpellChecker doesn't recognize. Do adjust according to the source pdf.
-names += [n+'s' for n in names] #Adding possesives
+names = ['caroline', 'timothy', 'mamba', 'glasslike'] # These are some words that occur in the book that SpellChecker doesn't recognize. Do adjust according to the source pdf.
+names += [n+'s' for n in names] # Adding possesives
 spell = SpellChecker()
 spell.word_frequency.load_words(names)
 
-#Doing a bit of complicated text cleanup of mess caused by pdfreader not interpreting linebreaks as anything.
+# Doing a bit of complicated text cleanup of mess caused by pdfreader not interpreting linebreaks as anything.
 
 def get_most_probable_split(word):
-	#This tests all ways of splitting a word into two and returns the most likely split as a list with lengt 2.
+	# This tests all ways of splitting a word into two and returns the most likely split as a list with lengt 2.
 	max_prob = 0
 	max_prob_i = 0
 	max_p1, max_p2 = 0, 0
@@ -73,11 +73,11 @@ def get_most_probable_split(word):
 			max_p1, max_p2 = p1, p2
 	return [word[:max_prob_i], word[max_prob_i:]]
 
-#Splitting inacurately joined words with the help of SpellChecker...
+# Splitting inacurately joined words with the help of SpellChecker...
 wrong_word_prob_cutoff = 10 ** (-7)
 
 def double_word_splitter(word,sep):
-	#This splits words that use some kind of separator, runs get_most_probable_split() on each sub-word, and then joins the words together again with the original separator.
+	# This splits words that use some kind of separator, runs get_most_probable_split() on each sub-word, and then joins the words together again with the original separator.
 	d_w = []
 	for sub_word in word.split(sep):
 		clean_sub_word = re.sub('[^a-zA-Z]+', '', sub_word).lower()
@@ -92,15 +92,15 @@ def double_word_splitter(word,sep):
 	return [d_w]
 
 s3 = []
-for word in s2.split(' '): #Looping through all the words in the book.
+for word in s2.split(' '): # Looping through all the words in the book.
 	clean_word = re.sub('[^a-zA-Z]+', '', word).lower()
 	if len(clean_word) > 0:
-		#Dealing with words with hyphens
+		# Dealing with words with hyphens
 		if '-' in word[:-1]:
 			s3 += double_word_splitter(word,'-')
 		elif '_' in word[:-1]:
 			s3 += double_word_splitter(word,'_')
-		#Dealing with non-hyphen words
+		# Dealing with non-hyphen words
 		else:
 			if spell.word_probability(clean_word) <= wrong_word_prob_cutoff:
 				s3 += get_most_probable_split(word)
@@ -123,7 +123,6 @@ if TEST:
 	voices = engine.getProperty('voices')
 	for voice in voices:
 		engine.setProperty('voice', voice.id)
-		#engine.say(txt[100:400])
 		engine.save_to_file(txt[200:1000],root_dir + 'test_' + voice.name + '.mp3')
 		engine.runAndWait()
 		sleep(1)
